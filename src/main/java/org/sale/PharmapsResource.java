546025d87ad4a64a56f08f7f0bb6035d0eb6f9ed
Josué DA-MATHA <josuedamatha2001@gmail.com>
2024-07-07 00:35:38 +0200

    add login fonctionality

--- src/main/java/org/sale/PharmapsResource.java
+++ src/main/java/org/sale/PharmapsResource.java
@@ -1,14 +1,14 @@
 package org.sale;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Map;
 
 @Path("/phmps")
 public class PharmapsResource {
@@ -31,6 +31,48 @@ public class PharmapsResource {
                 em.getTransaction().rollback();
             }
             return Response.serverError().entity("Erreur lors de l'inscription de l'utilisateur").build();
+        } finally {
+            em.close();
+        }
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loginUser(Map<String, String> credentials) {
+        String email = credentials.get("email");
+        String password = credentials.get("password");
+
+        try {
+            TypedQuery<User> query = em.createQuery(
+                    "SELECT u FROM User u WHERE u.email = :email AND u.password = :password", User.class);
+            query.setParameter("email", email);
+            query.setParameter("password", password);
+
+            User user = query.getSingleResult();
+
+            // Construire la réponse
+            return Response.ok()
+                    .entity(Map.of(
+                            "value", 1,
+                            "message", "Connexion reussi",
+                            "userId", user.getId(),
+                            "fullname", user.getFullname(),
+                            "email", user.getEmail()
+                    ))
+                    .build();
+        } catch (NoResultException e) {
+            // Utilisateur non trouvé
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(Map.of(
+                            "value", 0,
+                            "message", "Email ou mot de passe incorrect"
+                    ))
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().entity("Erreur lors de la connexion de l'utilisateur").build();
         } finally {
             em.close();
         }
